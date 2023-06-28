a062aeb3ed1b25079c6370c31c3ef421829e501d
karthikeka <karthikeka94@gmail.com>
2023-06-28 13:46:38 +0530

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import Dashboard from './DashBoard';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Create from "./Create";
+import Read from "./Read";
+import Update from "./Update";
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Create />}></Route>
+          <Route path="/read" element={<Read />}></Route>
+          <Route path="/update" element={<Update />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
